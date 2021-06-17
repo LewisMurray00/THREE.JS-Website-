92193737c8e75a588f9e18c51c53c0ccb1ac4b2b
Lewis Murray <lewisdmurray00@gmail.com>
2021-06-17 15:28:00 +0100

    Camera and render added

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 const scene = new THREE.Scene();
 
+//Perspective camera is what we would see through our eyes
+// 1st: FOV 2nd: Aspect ratio 3rd: view Frustum
+//CAMERA
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight);
 
+//RENDER
+//need to use a renderer, which is where the magic happens
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+//GEOMETRY
